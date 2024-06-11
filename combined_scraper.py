04c6f6df750a1a12af562f92ba7aa5ee2de2d93d
General <general@ctrinstitute.com>
2024-06-11 07:59:54 -0500

    Added topic scraping to combined scraper.

--- combined_scraper.py
+++ combined_scraper.py
@@ -383,6 +383,82 @@ def fetch_all_webinars():
         page_number += 1
     return all_webinars
 
+class TopicParser(HTMLParser):
+    """An HTMLParser class used to scrape Topic data."""
+
+    def __init__(self):
+        super().__init__()
+        self.topics = []
+        self.title_flag = False
+        self.current_url = None
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for attr in attrs:
+                if attr[0] == "href":
+                    self.current_url = attr[1]
+
+        if tag == "h3":
+            for attr in attrs:
+                if attr == ('class', 'h3 topic-card__title'):
+                    self.title_flag = True
+
+    def handle_data(self, data):
+        if self.title_flag and self.current_url:
+            title = data.strip()
+            if title.endswith('-'):
+                title = title.rstrip('-')
+            self.topics.append({"Title": title, "URL": self.current_url})
+            self.title_flag = False
+            self.current_url = None
+
+    def handle_endtag(self, tag):
+        if tag == "h3" and self.title_flag:
+            self.title_flag = False
+
+    def get_topics(self):
+        return self.topics
+
+def fetch_topic_page(page_number):
+    url = "https://ctrinstitute.com/wp-admin/admin-ajax.php?action=topic_archive_filter"
+    params = {
+        'paged': page_number,
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    # Check if the response content type is JSON or not
+    if 'application/json' in response.headers.get('Content-Type', ''):
+        json_response = response.json()
+        return json_response.get('data', {}).get('posts', '') 
+    else:
+        return response.text
+
+def fetch_all_topics():
+    page_number = 1
+    all_topics = []
+    while True:
+        print(f"Fetching Topic: page {page_number}...")
+        posts_html = fetch_topic_page(page_number)
+
+        if not posts_html:
+            break
+
+        parser = TopicParser()
+        parser.feed(posts_html)
+        topics = parser.get_topics()
+
+        if not topics:
+            break
+        all_topics.extend(topics)
+        page_number += 1
+    return all_topics
+
 # Create/find the excel spreadsheet
 def get_documents_path():
     return os.path.join(os.path.join(os.path.expanduser('~')), 'Documents')
@@ -405,6 +481,15 @@ try:
         
         print("**** SCRAPING DATA FROM CTRINSTITUTE.COM ****")
         print("This will take a minute...")
+
+        # Fetch and write topics.
+        topics = fetch_all_topics()
+        df_topics = pd.DataFrame(topics)
+        df_topics.to_excel(writer, sheet_name="Topics", index=False)
+        worksheet = writer.sheets['Topics']
+        for idx, url in enumerate(df_topics['URL'], start=2):
+            worksheet.write_url(f'B{idx}', url)
+
 
         # Fetch and write workshops.
         workshops = fetch_all_workshops()
