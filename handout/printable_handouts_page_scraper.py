76814d1e2ce7e174f4ad20856c66a02144337e66
General <jhamilton3@rrc.ca>
2024-07-03 09:01:06 -0500

    Printable handout page scraping working.

---
+++ handout/printable_handouts_page_scraper.py
@@ -0,0 +1,39 @@
+import requests
+from html.parser import HTMLParser
+import pandas as pd
+import cloudscraper
+
+class PrintableHandoutsPageScraper(HTMLParser):
+    """An HTMLParser class used to scrape Printable Handout page data."""
+
+    def __init__(self):
+        super().__init__()
+        self.resource_link = []
+
+        self.resource_link_flag = False
+        self.inside_right_p_tag = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "p":
+            for attr in attrs:
+                if attr[1] == "has-text-align-right":
+                    self.inside_right_p_tag = True
+
+        if tag == "a" and self.inside_right_p_tag:
+            for attr in attrs:
+                self.resource_link.append(attr[1])
+
+    def handle_endtag(self, tag):
+        if tag == "p":
+            self.inside_right_p_tag = False
+
+    def get_printable_handout_information(self):
+        return {"printable_handout_resource_link": ' '.join(self.resource_link)}
+        
+def fetch_printable_handout_description_page(url):
+    cloud_scraper = cloudscraper.create_scraper()
+    response = cloud_scraper.get(url)
+
+    html_content = response.content.decode('utf-8')
+
+    return html_content
